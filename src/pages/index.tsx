929720d7d87fc851cf5b267965c38a68135a3c25
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-02-07 10:19:44 +0000

    feat: colour picker and tabs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ChromePicker } from 'react-color';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -17,6 +18,8 @@ const IndexPage = () => {
     "",
     "",
   ]);
+  const [tab, setTab] = useState<number>(0);
+  const [colour, setColour] = useState<string>('#F1E5B2');
 
   return (
     <Layout>
@@ -24,6 +27,19 @@ const IndexPage = () => {
       <form>
         <div className="columns">
           <div className="column">
+            <div className="tabs">
+              <ul>
+                <li className={tab === 0 ? 'is-active' : undefined}><a href="#" onClick={(event) => {
+                  event.preventDefault();
+                  setTab(0);
+                }}>Players</a></li>
+                <li className={tab === 1 ? 'is-active' : undefined}><a href="#" onClick={(event) => {
+                  event.preventDefault();
+                  setTab(1);
+                }}>Styles</a></li>
+              </ul>
+            </div>
+            <div className={`tab${tab === 0 ? ' is-active' : ''}`}>
             {players.map((player, index) => (
               <div className="field has-addons">
                 <div className="control">
@@ -31,7 +47,7 @@ const IndexPage = () => {
                 </div>
                 <div className="control">
                   <input
-                  className="input"
+                    className="input"
                     key={index}
                     value={player}
                     onChange={event => {
@@ -45,9 +61,16 @@ const IndexPage = () => {
                 </div>
               </div>
             ))}
+            </div>
+            <div className={`tab${tab === 1 ? ' is-active' : ''}`}>
+              <label className="label">Team Colour</label>
+              <ChromePicker onChange={(colour) => {
+                setColour(colour.hex);
+              }} color={colour} />
+            </div>
           </div>
           <div className="column">
-            <div id="image-builder">
+            <div id="image-builder" style={{background: colour}}>
               <ul className="players">
                 {players.map((player) => (
                   <li>{player}</li>
